9f69cb7270bd462e50ed6fe6aa732e67a69ab53d
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-10-10 00:13:42 +0200

    Restructured test

--- __tests__/test.js
+++ __tests__/test.js
@@ -22,18 +22,13 @@ function absPathAppender(relPath) {
     return path.join(tmpDir, relPath);
 }
 
-describe('All lint files are created', () => {
+describe('All files are created', () => {
 	beforeEach(function () {
 	  // The object returned acts like a promise, so return it to wait until the process is done
 	  return helpers.run(path.join(__dirname, '../generators/app'))
 			.inTmpDir((dir) => {
 		    tmpDir = dir;
-			})
-	    .withOptions({ // Mock options passed in
-				appName: APP_TITLE,
-				packageName: APP_TITLE,
-				manager: DEP_MANAGER
-   	 	})
+			});
 	});
 
   eslintFilesPath.map(function(file) {
@@ -41,24 +36,49 @@ describe('All lint files are created', () => {
       assert.file(absPathAppender('/' + file));
     });
   });
-});
 
-describe('All configuration files are generated', () => {
-  it('creates Gruntfile', () => {
+  it('creates --> Gruntfile.js', () => {
     assert.file(absPathAppender('/Gruntfile.js'));
   });
 
-  it('creates package.json', () => {
+  it('creates --> package.json', () => {
     assert.file(absPathAppender('/package.json'));
   });
-});
-
-describe('All hidden files are generated', () => {
 
   hiddenFiles.map(function(file) {
     it('creates --> ' + file, () => {
       assert.file(absPathAppender('/' + file));
     });
   });
-
 });
+
+// describe('Files are created with the user answers', () => {
+//   beforeEach(() => {
+//     return helpers
+//       .run(require.resolve('../generators/readme'))
+//       .withOptions({
+//         appName: APP_TITLE,
+//         packageName: PACKAGE_NAME,
+//         manager: DEP_MANAGER
+//       })
+//       .on('ready', gen => {
+//         gen.fs.writeJSON(gen.destinationPath('package.json'), {
+//           license: 'MIT'
+//         });
+//       });
+//   });
+//
+//   it('creates and fill contents in package.json', () => {
+//     assert.file('package.json');
+//     console.log('package_name: ', PACKAGE_NAME);
+//     assert.fileContent('package.json', PACKAGE_NAME);
+//     // assert.fileContent('README.md', '> a cool project');
+//     // assert.fileContent('README.md', '$ npm install --save my-project');
+//     // assert.fileContent('README.md', 'MIT © [Yeoman](http://yeoman.io)');
+//     // assert.fileContent(
+//     //   'README.md',
+//     //   '[travis-image]: https://travis-ci.org/yeoman/my-project.svg?branch=master'
+//     // );
+//     // assert.fileContent('README.md', 'coveralls');
+//   });
+// });
